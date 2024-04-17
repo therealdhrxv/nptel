031f21a8f60137c6c301540b11fde42e56779af7
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2024-04-17 20:20:19 +0530

    feat: quiz-flow

--- app/(quiz)/quiz/[week]/page.tsx
+++ app/(quiz)/quiz/[week]/page.tsx
@@ -4,15 +4,7 @@ import React, { useState, useEffect } from "react";
 import { Home } from "lucide-react";
 import { useRouter } from "next/navigation";
 
-import ScoreModal from "@/components/score-card-modal";
-import { Question } from "@/types/Question";
 import { questionsByWeek } from "@/components/data/questions";
-
-interface QuizProps {
-	params: {
-		week: string;
-	};
-}
 
 const shuffleArray = (array: any[]) => {
 	for (let i = array.length - 1; i > 0; i--) {
@@ -21,22 +13,20 @@ const shuffleArray = (array: any[]) => {
 	}
 };
 
+interface QuizProps {
+	params: {
+		week: string;
+	};
+}
+
 const Quiz: React.FC<QuizProps> = ({ params }) => {
-	
-	const [currentQuestions, setCurrentQuestions] = useState<Question[]>([]);
-	const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
+	const [selectedAnswers, setSelectedAnswers] = useState<string[]>([]);
 	const [score, setScore] = useState<number>(0);
+	const [showScore, setShowScore] = useState<boolean>(false);
+	const [submitted, setSubmitted] = useState<boolean>(false);
 	const router = useRouter();
 
-	const [showModal, setShowModal] = useState(false);
-
 	const { week } = params;
-
-	const returnHome = () => {
-		if (typeof window !== "undefined") {
-			router.push("/");
-		}
-	};
 
 	useEffect(() => {
 		const selectedWeek = week;
@@ -45,52 +35,119 @@ const Quiz: React.FC<QuizProps> = ({ params }) => {
 			const shuffledQuestions = [...weekQuestions];
 			shuffleArray(shuffledQuestions);
 			shuffledQuestions.forEach((q) => shuffleArray(q.options));
-			setCurrentQuestions(shuffledQuestions);
+			setSelectedAnswers(
+				new Array(shuffledQuestions.length).fill("")
+			);
 		}
 	}, [week]);
 
-	const handleAnswer = (option: string) => {
-		if (option === currentQuestions[currentQuestionIndex].answer) {
-			setScore(score + 1);
+	const handleAnswer = (option: string, index: number) => {
+		if (!submitted) {
+			const newSelectedAnswers = [...selectedAnswers];
+			newSelectedAnswers[index] = option;
+			setSelectedAnswers(newSelectedAnswers);
 		}
-		if (currentQuestionIndex < currentQuestions.length - 1) {
-			setCurrentQuestionIndex(currentQuestionIndex + 1);
-		} else {
-			setShowModal(true);
+	};
+
+	const calculateScore = () => {
+		let newScore = 0;
+		questionsByWeek[week].forEach((question, index) => {
+			if (question.answer === selectedAnswers[index]) {
+				newScore++;
+			}
+		});
+		setScore(newScore);
+		setShowScore(true);
+		setSubmitted(true);
+	};
+
+	const returnHome = () => {
+		if (typeof window !== "undefined") {
+			router.push("/");
 		}
 	};
 
 	return (
 		<div className="p-2">
-			<div className="text-center text-xl font-light"> {`week ${week.slice(4)} assignment`} </div>
 			<div
-				className="cursor-pointer h-10 float-right mr-3 mt-3 md:mt-[10%] md:mr-[5%]"
-				onClick={() => returnHome()}
+				className="cursor-pointer h-10 float-right mr-3 mt-3 md:mr-[5%]"
+				onClick={returnHome}
 			>
 				<Home />
 			</div>
-			<div className="text-2xl md:text-3xl font-semibold p-7 md:p-10">
-				{currentQuestions[currentQuestionIndex]?.question}
-			</div>
-			<div className="grid md:grid-cols-2 gap-3 p-5">
-				{currentQuestions[currentQuestionIndex]?.options.map(
-					(option, index) => (
-						<div
-							key={index}
-							onClick={() => handleAnswer(option)}
-							className="cursor-pointer text-center mt-10 lg:mt-16 mx-7 md:mx-16 lg:mx-[25%] rounded-lg border border-gray-300 p-4 hover:bg-gray-100 transition duration-300"
-						>
-							{option}
+
+			<div className="text-center text-xl font-light underline mb-10">{`Week ${week.slice(
+				4
+			)} Assignment`}</div>
+
+			<div className="p-5 flex flex-col space-y-20">
+				{questionsByWeek[week]?.map((question, index) => (
+					<div key={index}>
+						<div className="font-semibold">
+							{question.question}
 						</div>
-					)
-				)}
+						<div className="grid md:grid-cols-4 md:gap-20 gap-5 mt-3">
+							{question.options.map(
+								(option, optionIndex) => {
+									const isSelected =
+										selectedAnswers[index] === option;
+									const isCorrect =
+										option === question.answer;
+									const isSubmitted = submitted;
+									const optionClass = `cursor-pointer text-center rounded-lg border p-4 hover:bg-slate-100 transition ${
+										isSubmitted
+											? isCorrect
+												? "bg-green-200"
+												: isSelected
+												? "bg-red-200"
+												: ""
+											: isSelected
+											? "bg-slate-300"
+											: ""
+									}`;
+									return (
+										<div
+											key={optionIndex}
+											onClick={() =>
+												handleAnswer(option, index)
+											}
+											className={optionClass}
+										>
+											{option}
+										</div>
+									);
+								}
+							)}
+						</div>
+					</div>
+				))}
 			</div>
-			{showModal && (
-				<ScoreModal
-					score={score}
-					totalQuestions={currentQuestions.length}
-					onClose={() => setShowModal(false)}
-				/>
+			{!submitted ? (
+				<div className="text-center mb-10">
+					<button
+						className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded mt-5"
+						onClick={calculateScore}
+					>
+						Submit
+					</button>
+				</div>
+			) : (
+				<div className="text-center mb-10">
+					<button
+						className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded mt-5"
+						onClick={returnHome}
+					>
+						Done
+					</button>
+				</div>
+			)}
+			{showScore && (
+				<div className="md:text-6xl text-3xl font-mono mb-10">
+					<div className="font-semibold">
+						{score === 10 ? "🎉" : ""} Your score is: {score}/
+						{10} {score === 10 ? "🎉" : ""}
+					</div>
+				</div>
 			)}
 		</div>
 	);
